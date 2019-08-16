2052ce21f5afacdba8abda841f1e4b732ef7446b
Martin <7252614+Lhoerion@users.noreply.github.com>
2019-08-16 22:33:39 +0200

    Added downloading of csharp and node modules

--- update.ps1
+++ update.ps1
@@ -9,84 +9,99 @@
 # Run Information: This script is run manually.
 # Dependencies: >=PowerShell 5.0
 #
-
+Param([Switch]$noBackup)
 Clear-Content -Path 'update.log' 2>&1 >$null
 [System.Collections.ArrayList]$files=@()
 function printAndLog($str, $type) {
-    $date = Get-Date -UFormat '%T'
-    if($type -eq 'ERR') {
-        "[$date][Error] $str" | Add-Content -Path 'update.log' -NoNewline -PassThru | Write-Host -NoNewline -ForegroundColor 'Red'
-    } elseif($type -eq 'WARN') {
-        "[$date][Warning] $str" | Add-Content -Path 'update.log' -NoNewline -PassThru | Write-Host -NoNewline -ForegroundColor 'Yellow'
-    } elseif($type -eq 'APP') {
-        "$str" | Add-Content -Path 'update.log' -NoNewline -PassThru | Write-Host -NoNewline
-    } else {
-        "[$date] $str" | Add-Content -Path 'update.log' -NoNewline -PassThru | Write-Host -NoNewline
-    }
+	$date = Get-Date -UFormat '%T'
+	if($type -eq 'ERR') {
+		"[$date][Error] $str" | Add-Content -Path 'update.log' -NoNewline -PassThru | Write-Host -NoNewline -ForegroundColor 'Red'
+	} elseif($type -eq 'WARN') {
+		"[$date][Warning] $str" | Add-Content -Path 'update.log' -NoNewline -PassThru | Write-Host -NoNewline -ForegroundColor 'Yellow'
+	} elseif($type -eq 'APP') {
+		"$str" | Add-Content -Path 'update.log' -NoNewline -PassThru | Write-Host -NoNewline
+	} else {
+		"[$date] $str" | Add-Content -Path 'update.log' -NoNewline -PassThru | Write-Host -NoNewline
+	}
 }
 function validateFiles() {
-    $script:files.Clear();
-    foreach($file in @('altv-server.exe','data/vehmodels.bin','data/vehmods.bin')) {
-        if(!(Test-Path -Path "./$file") -or ((Get-FileHash -Path "./$file" -Algorithm 'SHA1').Hash.ToLower() -ne $updateData.hashList."$file")) {
-            $script:files += $file;
-        }
-    }
-    if(!(Test-Path -Path "./start.ps1")) {
-        printAndLog "Server file ./start.ps1 not found, creating one . . . "
-        $result=(New-Item -Path './start.ps1' -Value "./altv-server.exe`n" -Force)
-        if($result) {
-            printAndLog "done`n" 'APP'
-        } else {
-            printAndLog "failed`n" 'APP'
-        }
-    }
-    if($localBuild -ne $remoteBuild) {
-        printAndLog "Server files update is available`n"
-    } elseif($files.Count -ne 0) {
-        printAndLog "Server files are invalidated/corrupted, $($script:files.Count) in total`n"
-    } else {
-        printAndLog "Server files are up-to-date, no action required`n"
-    }
+	$script:files.Clear();
+	foreach($file in ($updateData.hashList | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name)) {
+		if(!(Test-Path -Path "$file") -or ((Get-FileHash -Path "$file" -Algorithm 'SHA1').Hash.ToLower() -ne $updateData.hashList."$file"[0])) {
+			$script:files += $file;
+		}
+	}
+	if(!(Test-Path -Path "start.ps1")) {
+		printAndLog "Server file start.ps1 not found, creating one . . . "
+		$result=(New-Item -Path 'start.ps1' -Value ".\altv-server.exe`n" -Force)
+		if($result) {
+			printAndLog "done`n" 'APP'
+		} else {
+			printAndLog "failed`n" 'APP'
+		}
+	}
+	if($localBuild -ne $remoteBuild) {
+		printAndLog "Server files update is available`n"
+	} elseif($files.Count -ne 0) {
+		printAndLog "Server files are invalidated/corrupted, $($script:files.Count) in total`n"
+	} else {
+		printAndLog "Server files are up-to-date, no action required`n"
+	}
 
-    $localBuild=$remoteBuild
-    [ordered]@{branch=$localBranch;build=$localBuild} | ConvertTo-Json > './update.cfg'
+	$localBuild=$remoteBuild
+	[ordered]@{branch=$localBranch;build=$localBuild} | ConvertTo-Json > 'update.cfg'
 }
 function downloadFiles() {
-    if($script:files.Count -eq 0) {
-        return
-    }
-    foreach($file in $script:files) {
-        $parentDir=(Split-Path -Path "$file" -Parent)
-        printAndLog "Downloading file ./$file  . . . "
-        if(Test-Path -Path "./$file") {
-            Move-Item -Path "./$file" -Destination "./$file.old" -Force >$null
-        }
-        if(!(Test-Path -Path "./$parentDir")) {
-            New-Item -Path "./$parentDir" -ItemType 'Directory' -Force >$null
-        }
-        $progressPreference = 'silentlyContinue'
-        $result=(Invoke-WebRequest -Uri "https://cdn.altv.mp/server/$localBranch/x64_win32/$file" -UserAgent 'AltPublicAgent' -UseBasicParsing  -OutFile "./$file" -PassThru)
-        if($result.StatusCode -eq 200) {
-            printAndLog "done`n" 'APP'
-        } else {
-            printAndLog "failed`n" 'APP'
-        }
-        $progressPreference = 'Continue'
-    }
-    validateFiles
+	if($script:files.Count -eq 0) {
+		return
+	}
+	foreach($file in $script:files) {
+		$dlType=($updateData.hashList."$file"[1])
+		$outDir=(Split-Path -Path "$file" -Parent).Replace('/', '\')
+		if($outDir -eq '') { $outDir='.' }
+		$fileName=(Split-Path -Path "$file" -Leaf)
+		if($dlType -ne 'server' -and $dlType -ne 'node-module') { $file=$fileName }
+		printAndLog "Downloading file $outDir\$fileName . . . "
+		if(!$noBackup -and (Test-Path -Path "$file")) {
+			Move-Item -Path "$file" -Destination "$file.old" -Force >$null
+		}
+		if(!(Test-Path -Path "$outDir")) {
+			New-Item -Path "$outDir" -ItemType 'Directory' -Force >$null
+		}
+		$progressPreference = 'silentlyContinue'
+		$result=(Invoke-WebRequest -Uri "https://cdn.altv.mp/$dlType/$localBranch/x64_win32/$file" -UserAgent 'AltPublicAgent' -UseBasicParsing  -OutFile "$outDir\$fileName" -PassThru)
+		if($result.StatusCode -eq 200) {
+			printAndLog "done`n" 'APP'
+		} else {
+			printAndLog "failed`n" 'APP'
+		}
+		$progressPreference = 'Continue'
+	}
+	validateFiles
 }
-if(!(Test-Path './update.cfg')) {
-    New-Item './update.cfg'  -Force >$null
-    [ordered]@{branch='stable'} | ConvertTo-Json > './update.cfg'
+if(!(Test-Path 'update.cfg')) {
+	New-Item 'update.cfg'  -Force >$null
+	[ordered]@{branch='stable'} | ConvertTo-Json > 'update.cfg'
 }
-$updateCfg=$(Get-Content './update.cfg' | ConvertFrom-Json)
+$updateCfg=$(Get-Content 'update.cfg' | ConvertFrom-Json)
 $localBranch=$updateCfg.branch
 if(!$localBranch -or $localBranch -ne 'stable' -and $localBranch -ne 'beta' -and $localBranch -ne 'alpha') { $localBranch='stable' }
 try {
-    $updateData=(Invoke-RestMethod -Uri "https://cdn.altv.mp/server/$localBranch/x64_win32/update.json" -UserAgent 'AltPublicAgent')
+	$updateData=(Invoke-RestMethod -Uri "https://cdn.altv.mp/server/$localBranch/x64_win32/update.json" -UserAgent 'AltPublicAgent')
+	$hashTable = @{}
+	$updateData.hashList.psobject.properties | Foreach { $hashTable[$_.Name] = @($_.Value,'server') }
+	if($localBranch -ne 'stable') {
+		$updateData2=(Invoke-RestMethod -Uri "https://cdn.altv.mp/node-module/$localBranch/x64_win32/update.json" -UserAgent 'AltPublicAgent')
+		$updateData3=(Invoke-RestMethod -Uri "https://cdn.altv.mp/coreclr-module/$localBranch/x64_win32/update.json" -UserAgent 'AltPublicAgent')
+		$updateData2.hashList.psobject.properties | Foreach { $hashTable[$_.Name] = @($_.Value,'node-module') }
+		$updateData3.hashList.psobject.properties | Foreach { $hashTable["modules/$($_.Name)"] = @($_.Value,'coreclr-module') }
+	} else {
+		printAndLog "Checking for update of csharp-module and node-module is not possible yet in stable branch`n" 'WARN'
+	}
+	$updateData.hashList = [pscustomobject]$hashTable;
 } catch {
-    printAndLog "Failed to check for update, try again later`n" 'ERR'
-    exit
+	printAndLog "Failed to check for update, try again later`n" 'ERR'
+	exit
 }
 $remoteBuild=$updateData.latestBuildNumber
 $localBuild=$updateCfg.build
--- update.sh
+++ update.sh
@@ -11,6 +11,14 @@
 # Dependencies: coreutils, wget, >=jq_1.4
 #
 
+noBackup=false
+for arg in "$@"
+do
+	if [ $arg = '-noBackup' ]; then
+		noBackup=true
+		break
+	fi
+done
 truncate -s 0 'update.log'
 files=()
 printAndLog() {
@@ -26,27 +34,27 @@ printAndLog() {
 }
 validateFiles() {
 	files=()
-	for file in {'altv-server','data/vehmodels.bin','data/vehmods.bin'}
+	for file in $(echo $updateData | jq -r '.hashList | keys[]')
 	do
-		if [[ ! -e "./$file" || $(sha1sum "./$file" |awk '{print $1}') != $(echo "${updateData}" |jq -r ".hashList.\"$file\"") ]]; then
+		if [[ ! -e "$file" || $(sha1sum "$file" | awk '{print $1}') != "$(echo "${updateData}" | jq -r ".hashList.\"$file\"[0]")" ]]; then
 			files+=("$file")
 		fi
 	done
-	if [ ! -e './start.sh' ]; then
-		printAndLog "Server file ./start.sh not found, creating one . . . "
-		printf '#!/bin/bash\nBASEDIR=$(dirname $0)\nexport LD_LIBRARY_PATH=${BASEDIR}\n./altv-server\n' > './start.sh' && printAndLog 'done\n' 'APP' || printAndLog 'failed\n' 'APP'
-		chmod +x './start.sh' || printAndLog "[$(date +%T)][Error] Failed to add execution permissions to file ./start.sh\e[39m\n" 'ERR'
+	if [ ! -e 'start.sh' ]; then
+		printAndLog "Server file start.sh not found, creating one . . . "
+		printf '#!/bin/bash\nBASEDIR=$(dirname $0)\nexport LD_LIBRARY_PATH=${BASEDIR}\n./altv-server\n' > 'start.sh' && printAndLog 'done\n' 'APP' || printAndLog 'failed\n' 'APP'
+		chmod +x 'start.sh' || printAndLog "[$(date +%T)][Error] Failed to add execution permissions to file start.sh\e[39m\n" 'ERR'
 	fi
 	if [ $localBuild -ne $remoteBuild ]; then
-    	printAndLog "Server files update is available\n"
+		printAndLog "Server files update is available\n"
 	elif [ "${#files[@]}" -ne 0 ]; then
 		printAndLog "Server files are invalidated/corrupted, ${#files[@]} in total\n"
 	else
 		printAndLog "Server files are up-to-date, no action required\n"
 	fi
 
-	localBuild="$remoteBuild"
-	jq -n --arg branch $localBranch --arg build $remoteBuild '{"branch":$branch,"build":$build}' > './update.cfg'
+	localBuild=$remoteBuild
+	printf '{"branch":"%s","build":%d}' $localBranch $remoteBuild | jq '.' > 'update.cfg'
 }
 downloadFiles() {
 	if [ "${#files[@]}" -eq 0 ]; then
@@ -54,40 +62,73 @@ downloadFiles() {
 	fi
 	for file in ${files[@]}
 	do
-		parentDir=$(dirname "$file")
-		printAndLog "Downloading file ./$file  . . . "
-		if [ -e "./$file" ]; then
-			mv "./$file" "./$file.old"
+		dlType="$(echo "${updateData}" | jq -r ".hashList.\"$file\"[1]")"
+		outDir="$(dirname $file)"
+		fileName="$(basename $file)"
+		if [[ $dlType != 'server' && $dlType != 'node-module' ]]; then
+			file="$(basename $file)"
 		fi
-		wget "https://cdn.altv.mp/server/$localBranch/x64_linux/$file" -U 'AltPublicAgent' -P "./$parentDir/" -q && printAndLog 'done\n' 'APP' || printAndLog 'failed\n' 'APP'
-		if [ ! -e "./$file" ]; then
+		printAndLog "Downloading file $outDir/$fileName . . . "
+		if [[ "$noBackup" == 'false' && -e "$file" ]]; then
+			mv "$file" "$file.old"
+		fi
+		wget "https://cdn.altv.mp/$dlType/$localBranch/x64_linux/$file" -U 'AltPublicAgent' -P "$outDir/" -N -q && printAndLog 'done\n' 'APP' || printAndLog 'failed\n' 'APP'
+		if [ ! -e "$file" ]; then
 			continue
 		fi
-		if [ -e "./$file.old" ]; then
-			chmod --reference="./$file.old" "./$file" || printAndLog "Failed to copy chmod to file ./$file\n" 'ERR'
-			chmod -x "./$file.old" || printAndLog "Failed to remove execution permissions from file ./$file.old\n" 'ERR'
+		if [ -e "$file.old" ]; then
+			chmod --reference="$file.old" "$file" || printAndLog "Failed to copy chmod to file $file\n" 'ERR'
+			chmod -x "$file.old" || printAndLog "Failed to remove execution permissions from file $file.old\n" 'ERR'
 		else
-			chmod +x "./$file" || printAndLog "Failed to add execution permissions to file ./$file\n" 'ERR'
+			chmod +x "$file" || printAndLog "Failed to add execution permissions to file $file\n" 'ERR'
 		fi
 	done
 	validateFiles
 }
 
-if [ ! -e './update.cfg' ]; then
-	touch './update.cfg'
-	jq -n --arg branch stable '{"branch":"$branch"}' > './update.cfg'
+if [ ! -e 'update.cfg' ]; then
+	printf '{"branch":"stable"}' | jq '.' > 'update.cfg'
 fi
-updateCfg=$(cat './update.cfg' |jq -r '.')
-localBranch=$(echo "${updateCfg}" |jq -r '.branch')
+updateCfg=$(cat 'update.cfg' | jq '.')
+localBranch=$(echo "${updateCfg}" | jq -r '.branch')
 [[ ! -n "$localBranch" || "$localBranch" != 'stable' && "$localBranch" != 'beta' && "$localBranch" != 'alpha' ]] && localBranch='stable'
 updateData=$(curl -s "https://cdn.altv.mp/server/$localBranch/x64_linux/update.json" -A 'AltPublicAgent')
-echo $updateData |jq empty 2>/dev/null
+echo $updateData | jq empty 2>/dev/null
 if [ $? -ne 0 ]; then
 	printAndLog "Failed to check for update, try again later\n" 'ERR'
 	exit
 fi
-remoteBuild=$(echo "${updateData}" |jq -r '.latestBuildNumber')
-localBuild=$(echo "${updateCfg}" |jq -r '.build')
-[[ ! "$localBuild" =~ ^[0-9]+$ ]] && localBuild="$remoteBuild"
+str='. | to_entries | map(if .key=="hashList" then {"key":.key} + {"value":(.value | to_entries | map(. + {"key":"%s\(.key)","value":[.value, "%s"]}) | from_entries)} else . end) | from_entries'
+updateTmp=$(mktemp '/tmp/update.sh.XXX') && echo '{}' > $updateTmp 
+updateTmp2=$(mktemp '/tmp/update.sh.XXX') && echo '{}' > $updateTmp2
+updateTmp3=$(mktemp '/tmp/update.sh.XXX') && echo '{}' > $updateTmp3
+echo $updateData | jq -c "$(printf "$str" '' 'server')" > $updateTmp
+if [ "$localBranch" == 'beta' ]; then
+	updateData2=$(curl -s "https://cdn.altv.mp/node-module/$localBranch/x64_linux/update.json" -A 'AltPublicAgent')
+	echo $updateData2 | jq empty 2>/dev/null
+	if [ $? -ne 0 ]; then
+		printAndLog "Failed to check for update, try again later\n" 'ERR'
+		exit
+	fi
+	updateData3=$(curl -s "https://cdn.altv.mp/coreclr-module/$localBranch/x64_linux/update.json" -A 'AltPublicAgent')
+	echo $updateData3 | jq empty 2>/dev/null
+	if [ $? -ne 0 ]; then
+		printAndLog "Failed to check for update, try again later\n" 'ERR'
+		exit
+	fi
+	echo $updateData2 | jq -c "$(printf "$str" '' 'node-module')" > $updateTmp2
+	echo $updateData3 | jq -c "$(printf "$str" 'modules/' 'coreclr-module')" > $updateTmp3
+	unset updateData2
+	unset updateData3
+else
+	printAndLog "Checking for update of csharp-module and node-module is not possible yet in current branch\n" 'WARN'
+fi
+updateData=$(jq -s '.[1] * .[2] * .[0] | .hashList |= (to_entries | sort_by(.key) | from_entries) | .' $updateTmp $updateTmp2 $updateTmp3)
+rm $updateTmp
+rm $updateTmp2
+rm $updateTmp3
+remoteBuild=$(echo "${updateData}" | jq -r '.latestBuildNumber')
+localBuild=$(echo "${updateCfg}" | jq -r '.build')
+[[ ! "$localBuild" =~ ^[0-9]+$ ]] && localBuild=$remoteBuild
 validateFiles
 downloadFiles
